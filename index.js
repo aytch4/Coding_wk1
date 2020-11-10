648ca4f70906a520c83c39bef18d3e1a3ab1ce87
aytch4 <73347541+aytch4@users.noreply.github.com>
2020-11-10 10:25:17 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+var cokePrice = 1;
+var chipsPrice = 1.50;
+
+var cashOnHand = 5;
+var moneyInWallet = 20;
+
+var benFriends = 10;
+var benBff = 1;
+
+var benAge = 10;
+var dylanAge = 7;
+
+var myFirstName = "Heather";
+var sisterFirstName = "Laurie";
+
+var myLastName = "Staples";
+var sisterLastName = "Mikkola";
+
+var myMI = "L";
+var sisterMI = "B";
+
+var moneyNow = moneyInWallet - cokePrice;
+var friendsPerYear = benAge / benFriends;
+var myFullName = myFirstName + " " + myMI + " " + myLastName;
+
+console.log("Coke costs $" + cokePrice);
+console.log("Chips are $" + chipsPrice + "0");
+console.log("I have $" + cashOnHand);
+console.log("I wish I had $" + moneyInWallet);
+console.log("Ben has " + benFriends + " friends.");
+console.log("Ben has " + benBff + " best friend.");
+console.log("Ben is " + benAge + " years old.");
+console.log("Dylan is " + dylanAge);
+console.log("My name is " + myFirstName +" " + myMI + " " +myLastName);
+console.log("My sister's name is " + sisterFirstName + " " + sisterMI + " " + sisterLastName);
+console.log("If I bought a Coke with the money I wish I had in my wallet, I'd have $" + moneyNow + " left.");
+console.log("Ben has made " + friendsPerYear + " friend per year of his life.");
+console.log("My full name is " + myFullName);
